a9d2a14f731de88abdcc79bb213bdfc9aeecb64a
kawindme <github@windmeier.de>
2017-12-18 22:27:11 +0100

    #42 update compile_docs.sh -  mv cannot overwrite non-empty subfolders

--- compile_docs.sh
+++ compile_docs.sh
@@ -5,8 +5,11 @@ cd enviro/compute_docs/
 make html
 cd ../..
 
+# empty out because mv cannot overwrite non empty sub-folders
+rm -rf out/*
+
 # move to /out dir
-mv -f enviro/compute_docs/_build/html/ out
+mv -f enviro/compute_docs/_build/html/* out
 
 # delete unnecessary stuff in _build folder
 rm -rf enviro/compute_docs/_build/
