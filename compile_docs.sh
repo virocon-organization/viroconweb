30663aa83615f5801c2b3df162bea7bd64ce5396
kawindme <github@windmeier.de>
2017-12-18 22:38:27 +0100

    #24 comment rm commands

--- compile_docs.sh
+++ compile_docs.sh
@@ -12,10 +12,10 @@ rm -rf out/*
 mv -f enviro/compute_docs/_build/html/* out
 
 # delete unnecessary stuff in _build folder
-rm -rf enviro/compute_docs/_build/
+#rm -rf enviro/compute_docs/_build/
 
 # create .nojekyll-file
 touch out/.nojekyll
 
 # delete .buildinfo
-rm -rf out/.buildinfo
+#rm -rf out/.buildinfo
