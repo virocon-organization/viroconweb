ab7ca463c26c351da9e26b75ad15f2baa8ee4450
kawindme <github@windmeier.de>
2017-12-19 10:05:31 +0100

    #24 make sure out folder exists, before copying files into it

--- compile_docs.sh
+++ compile_docs.sh
@@ -5,10 +5,13 @@ cd enviro/compute_docs/
 make html
 cd ../..
 
+# create out dir, if it does not exist yet
+mkdir -p out
+
 # empty out because mv cannot overwrite non empty sub-folders
 rm -rf out/*
 
-# move to /out dir
+# move to out dir
 mv -f enviro/compute_docs/_build/html/* out
 
 # delete unnecessary stuff in _build folder
