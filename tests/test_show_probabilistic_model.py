00d76fa9c41c38aa9cfc82ce0cf600eb76db4ceb
Andreas <a.haselsteiner@uni-bremen.de>
2018-02-07 16:52:15 +0100

    implement test showing a probabilistic model

--- tests/test_show_probabilistic_model.py
+++ tests/test_show_probabilistic_model.py
@@ -4,81 +4,58 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 import os
 from enviro.forms import VariableNumber, VariablesForm
 
+
 class ShowProbModelTestCase(TestCase):
     def setUp(self):
         # create a user
         self.client = Client()
         self.client.post(reverse('user:create'),
-                           {'username' : 'max_mustermann',
-                            'email': 'max.mustermann@gmail.com',
-                            'first_name': 'Max',
-                            'last_name': 'Mustermann',
-                            'organisation': 'Musterfirma',
-                            'type_of_use': 'commercial',
-                            'password1' : 'AnJaKaTo2018',
-                            'password2': 'AnJaKaTo2018'})
+                         {'username': 'max_mustermann',
+                          'email': 'max.mustermann@gmail.com',
+                          'first_name': 'Max',
+                          'last_name': 'Mustermann',
+                          'organisation': 'Musterfirma',
+                          'type_of_use': 'commercial',
+                          'password1': 'AnJaKaTo2018',
+                          'password2': 'AnJaKaTo2018'})
 
-        # create a measurement file
-        test_files_path = os.path.abspath(os.path.join(
-            os.path.dirname( __file__), r'test_files/'))
-        file_name = '1yeardata_vanem2012pdf_withHeader.csv'
-
-        # thanks to: https://stackoverflow.com/questions/2473392/unit-testing-
-        # a-django-form-with-a-filefield
-        test_file = open(os.path.join(test_files_path , file_name), 'rb')
-        test_file_simple_uploaded = SimpleUploadedFile(test_file.name,
-                                                       test_file.read())
-        self.client.post(reverse('enviro:measurefiles-add'),
-                                    {'title' : file_name,
-                                     'measure_file' : test_file_simple_uploaded
-                                    })
-
-       # open direct input url and check if the html is correct
-        response = self.client.post(reverse('enviro:probabilistic_model-add',
-                                            args=['02']))
-        self.assertContains(response, "The first characer should be "
-                                      "capizalized.", status_code=200)
-
-        # create a direct input form
-        form = VariableNumber({'variable_number' : '2'})
-        self.assertTrue(form.is_valid())
-
-        # create a form containing the information of the model
+        # create a form containing the information of the  probabilistic model
         form_input_dict = {
-                'variable_name_0' : 'significant wave height [m]',
-                'variable_symbol_0' : 'Hs',
-                'distribution_0' : 'Weibull',
-                'scale_0_0' : '2.776',
-                'shape_0_0' : '1.471',
-                'location_0_0' : '0.888',
-                'variable_name_1' : 'peak period [s]',
-                'variable_symbol_1' : 'Tp',
-                'distribution_1': 'Weibull',
-                'scale_dependency_1': '0f2',
-                'scale_1_0' : '0.1',
-                'scale_1_1' : '1.489',
-                'scale_1_2' : '0.1901',
-                'shape_dependency_1' : '0f1',
-                'shape_1_0' : '0.04',
-                'shape_1_1' : '0.1748',
-                'shape_1_2' : '-0.2243',
-                'location_dependency_1': '!None',
-                'location_1_0' : '0',
-                'location_1_1' : '0',
-                'location_1_2' : '0',
-                'collection_name' : 'direct input Vanem2012'
-            }
-        form = VariablesForm(data=form_input_dict, variable_count=2)
-        self.assertTrue(form.is_valid())
+            'variable_name_0': 'significant wave height [m]',
+            'variable_symbol_0': 'Hs',
+            'distribution_0': 'Weibull',
+            'scale_0_0': '2.776',
+            'shape_0_0': '1.471',
+            'location_0_0': '0.888',
+            'variable_name_1': 'peak period [s]',
+            'variable_symbol_1': 'Tp',
+            'distribution_1': 'Weibull',
+            'scale_dependency_1': '0f2',
+            'scale_1_0': '0.1',
+            'scale_1_1': '1.489',
+            'scale_1_2': '0.1901',
+            'shape_dependency_1': '0f1',
+            'shape_1_0': '0.04',
+            'shape_1_1': '0.1748',
+            'shape_1_2': '-0.2243',
+            'location_dependency_1': '!None',
+            'location_1_0': '0',
+            'location_1_1': '0',
+            'location_1_2': '0',
+            'collection_name': 'direct input Vanem2012'
+        }
 
-        # test the view method and the html which will be generated
-        response = self.client.post(reverse('enviro:probabilistic_model-add',
-                                            args=['02']),
-                                    form_input_dict,
-                                    follow=True)
-        self.assertContains(response, "ploaded probabilistic models",
-                            status_code=200)
-
+        # create a probabilistic model
+        self.client.post(reverse('enviro:probabilistic_model-add',
+                                 args=['02']),
+                         form_input_dict,
+                         follow=True)
 
     def test_show_prob_model(self):
-
+        response = self.client.post(reverse('enviro:probabilistic_model-show',
+                                            kwargs={'pk': 1}),
+                                    follow=True)
+        # check if the H of H_s is formated by latexify
+        self.assertContains(response, '<span class="django-latexify'
+                                      ' math inline">f_{H_{s}}(h_{s})=',
+                            status_code=200)
