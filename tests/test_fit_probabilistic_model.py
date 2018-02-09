0a36d7dd1a9f6faf0979b8cb664eaa33f00208bb
Andreas <a.haselsteiner@uni-bremen.de>
2018-02-09 13:57:33 +0100

    implene tests which cover fitting models with no dependencies, these caused errors before, #52

--- tests/test_fit_probabilistic_model.py
+++ tests/test_fit_probabilistic_model.py
@@ -7,7 +7,7 @@ from enviro.forms import MeasureFileFitForm
 
 class FitProbModelTestCase(TestCase):
     def setUp(self):
-        # create a user
+        # Create a user
         self.client = Client()
         self.client.post(reverse('user:create'),
                            {'username' : 'max_mustermann',
@@ -19,11 +19,13 @@ class FitProbModelTestCase(TestCase):
                             'password1' : 'AnJaKaTo2018',
                             'password2': 'AnJaKaTo2018'})
 
-        # create a measurement file
+
+    def test_fit_probabilistic_model_vanem2012(self):
+        # Create a measurement file
         test_files_path = os.path.abspath(os.path.join(os.path.dirname( __file__), r'test_files/'))
         file_name = '1yeardata_vanem2012pdf_withHeader.csv'
 
-        # thanks to: https://stackoverflow.com/questions/2473392/unit-testing-
+        # Thanks to: https://stackoverflow.com/questions/2473392/unit-testing-
         # a-django-form-with-a-filefield
         test_file = open(os.path.join(test_files_path , file_name), 'rb')
         test_file_simple_uploaded = SimpleUploadedFile(test_file.name,
@@ -33,19 +35,17 @@ class FitProbModelTestCase(TestCase):
                                      'measure_file' : test_file_simple_uploaded
                                     })
 
-
-    def test_fit_probabilistic_model(self):
-        # open fitting url and check if the html is correct
+        # Open fitting url and check if the html is correct
         response = self.client.get(reverse('enviro:measurefiles-fit',
                                             kwargs={'pk' : 1}))
         self.assertContains(response, "example_normal.svg", status_code=200)
 
-        # create a fitting form without input
+        # Create a fitting form without input
         form = MeasureFileFitForm(
             variable_count=2,
             variable_names=['significant wave height [m]', 'peak period [s]'])
 
-        # create a fitting form with input (Vanem2012 model, like the test data)
+        # Create a fitting form with input (Vanem2012 model, like the test data)
         form_input_dict = {
                 'title' : 'Test fit',
                 '_significant wave height [m]' : 'significant wave height [m]',
@@ -63,7 +63,85 @@ class FitProbModelTestCase(TestCase):
             variable_count=2)
         self.assertTrue(form.is_valid())
 
-        # test if the fit worked and the correct view is shown
+        # Test if the fit worked and the correct view is shown
+        response = self.client.post(reverse('enviro:measurefiles-fit',
+                                            kwargs={'pk' : 1}),
+                                    form_input_dict,
+                                    follow=True)
+        self.assertContains(response, "Visual inspection", status_code=200)
+
+        # Finally delete the uploaded file. This servers two purposes:
+        # 1. To test it
+        # 2. To avoid adding up .csv files in the dir each time the test is run
+        response = self.client.get(reverse('enviro:measurefiles-delete',
+                                           kwargs={'pk': 1}),
+                                   follow=True)
+        self.assertContains(response, "ploaded measurement files",
+                            status_code = 200)
+
+
+    def test_fit_probabilistic_model_no_dependency(self):
+        # Create a measurement file
+        test_files_path = os.path.abspath(os.path.join(os.path.dirname( __file__), r'test_files/'))
+        file_name = '1yeardata_vanem2012pdf_withHeader.csv'
+
+        # Thanks to: https://stackoverflow.com/questions/2473392/unit-testing-
+        # a-django-form-with-a-filefield
+        test_file = open(os.path.join(test_files_path , file_name), 'rb')
+        test_file_simple_uploaded = SimpleUploadedFile(test_file.name,
+                                                       test_file.read())
+        self.client.post(reverse('enviro:measurefiles-add'),
+                                    {'title' : file_name,
+                                     'measure_file' : test_file_simple_uploaded
+                                    })
+        # --- WEIBULL-WEIBULL ---
+        # Create a fitting form with the input of a Weibull-Weibull model,
+        # which has no no depedencies
+        form_input_dict = {
+                'title' : 'Weibull Weibull, interval width: 2',
+                '_significant wave height [m]' : 'significant wave height [m]',
+                'distribution_0' : 'Weibull',
+                'width_of_intervals_0' : '2',
+                '_peak period [s]': 'peak period [s]',
+                'distribution_1' : 'Weibull',
+                'scale_dependency_1' : '!None',
+                'shape_dependency_1' : '!None',
+                'location_dependency_1' : '!None'
+            }
+        form = MeasureFileFitForm(
+            data=form_input_dict,
+            variable_names=['significant wave height [m]', 'peak period [s]'],
+            variable_count=2)
+        self.assertTrue(form.is_valid())
+
+        # Test if the fit worked and the correct view is shown.
+        response = self.client.post(reverse('enviro:measurefiles-fit',
+                                            kwargs={'pk' : 1}),
+                                    form_input_dict,
+                                    follow=True)
+        self.assertContains(response, "Visual inspection", status_code=200)
+
+        # --- WEIBULL-NORMAL ---
+        # Create a fitting form with the input of a Weibull-Normal model,
+        # which has no no depedencies
+        form_input_dict = {
+                'title' : 'Weibull Normal, interval width: 2',
+                '_significant wave height [m]' : 'significant wave height [m]',
+                'distribution_0' : 'Weibull',
+                'width_of_intervals_0' : '2',
+                '_peak period [s]': 'peak period [s]',
+                'distribution_1' : 'Normal',
+                'scale_dependency_1' : '!None',
+                'shape_dependency_1' : '!None',
+                'location_dependency_1' : '!None'
+            }
+        form = MeasureFileFitForm(
+            data=form_input_dict,
+            variable_names=['significant wave height [m]', 'peak period [s]'],
+            variable_count=2)
+        self.assertTrue(form.is_valid())
+
+        # Test if the fit worked and the correct view is shown
         response = self.client.post(reverse('enviro:measurefiles-fit',
                                             kwargs={'pk' : 1}),
                                     form_input_dict,
