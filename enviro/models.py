899efd2f5b3e07aef785145ab1add2d49dae992e
Andreas <a.haselsteiner@uni-bremen.de>
2018-02-13 11:45:40 +0100

    Improve code style, add comments to all models.py methods, #42

--- enviro/models.py
+++ enviro/models.py
@@ -8,6 +8,13 @@ from django.core.exceptions import ValidationError
 from decimal import Decimal
 
 class MeasureFileModel(models.Model):
+    """
+    Model for a file containing measurement data.
+
+    A MeasureFileModel object holds a measurement file. It is associated to a
+    user (owner) and can be shared with other users.
+    """
+
     secondary_user = models.ManyToManyField(User, related_name="secondary", max_length=50)
     primary_user = models.ForeignKey(User, null=True, related_name="primary")
     title = models.CharField(max_length=50, default="MeasureFile")
@@ -22,18 +29,34 @@ class MeasureFileModel(models.Model):
 @receiver(models.signals.post_delete, sender=MeasureFileModel)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
     """
-    Deletes file from filesystem when corresponding `MediaFile` object is deleted.
-    :param sender: 
-    :param instance:       one item form the MeasureFile database. 
-    :param kwargs: 
-    :return: 
+    Deletes a file when the corresponding `MediaFile` object is deleted.
+
+    Parameters
+    ----------
+    sender : ?,
+        Description.
+    instance : ?,
+        Description
+    kwargs : ?
+        Description
     """
+
     if instance.measure_file:
         if os.path.isfile(instance.measure_file.path):
             os.remove(instance.measure_file.path)
 
 
 class ProbabilisticModel(models.Model):
+    """
+    Model for a multivariate distribution, e.g. sig. wave height and peak period
+
+    A ProbabilisticModel object is associated to a user (owner) and can be
+    shared with other users. It has a name and can be connected to
+    measurement data.
+    If a probabilistic model is defined, in addition X DistributionModel objects
+    are needed to define the distributions.
+    """
+
     primary_user = models.ForeignKey(User, null=True, related_name="variables_primary")
     secondary_user = models.ManyToManyField(User, related_name="variables_secondary")
     upload_date = models.DateTimeField(default=timezone.now)
@@ -44,6 +67,16 @@ class ProbabilisticModel(models.Model):
 
 
 class DistributionModel(models.Model):
+    """
+    Model for the distribution of a single random variable.
+
+    For example the random variable significant wave height, can be defined
+    with this model. It's name would be 'significant wave height', its
+    symbol 'Hs' and its distribution 'Weibull'. In addition one would need 3
+    ParameterModel objects, which define the distributions parameters (scale,
+    shape, location).
+    """
+
     DISTRIBUTIONS = (('Normal', 'Normal Distribution'), ('Weibull', 'Weibull'),
                      ('Lognormal_2', 'Log-Normal'), ('KernelDensity', 'Kernel Density'))
     name = models.CharField(default="peak period", max_length=50)
@@ -56,6 +89,15 @@ class DistributionModel(models.Model):
 
 
 class ParameterModel(models.Model):
+    """
+    Model for one parameter of a distribution, e.g. scale.
+
+    The parameter model can either be a constant value, e.g. x0 = 1.5 or
+    it can be a function, which describes the parameter's depedency on
+    another variable. The two available funtions have 3 parameters each, which
+    are represented by x0, x1 and x2.
+    """
+
     FUNCTIONS = ((None, 'None'), ('f1', 'power function'),
                  ('f2', 'exponential'))
     function = models.CharField(choices=FUNCTIONS, max_length=6)
@@ -73,41 +115,37 @@ class ParameterModel(models.Model):
 
     def clean(self):
         """
-        Validates that the distribution's parameters have valid values.
+        Validates if the parameter of a distribution has valid values.
 
-        All distribution's parameters are validated. For example a Normal
-        distribution's scale parameter (sigma) must be > 0. If this is not
-        the case, a ValidationError is raised.
+        For example a Normal distribution's scale parameter (sigma) must
+        be > 0. If this is not the case, a ValidationError is raised.
         """
 
+        # If the parameters are given as constant values
         if self.function == 'None':
-            if self.distribution.distribution == 'Normal' and \
-                            self.name == 'scale' and \
-                            self.x0 <= 0:
+            pass
+        if self.distribution.distribution == 'Normal':
+            if self.name == 'scale' and self.x0 <= 0:
                 raise ValidationError(
                     "The Normal distribution's scale parameter, sigma, "
                     "must be > 0.")
-            elif self.distribution.distribution == 'Weibull' and \
-                            self.name == 'scale' and \
-                            self.x0 <= 0:
+        elif self.distribution.distribution == 'Weibull':
+            if self.name == 'scale' and self.x0 <= 0:
                 raise ValidationError(
                     "The Weibull distribution's scale parameter, lambda, "
                     "must be > 0.")
-            elif self.distribution.distribution == 'Weibull' and \
-                            self.name == 'shape' and \
-                            self.x0 <= 0:
+            elif self.name == 'shape' and self.x0 <= 0:
                 raise ValidationError(
                     "The Weibull distribution's shape parameter, k, "
                     "must be > 0.")
-            elif self.distribution.distribution == 'Lognormal_2' and \
-                            self.name == 'shape' and \
-                            self.x0 <= 0:
+        elif self.distribution.distribution == 'Lognormal_2':
+            if self.name == 'shape' and self.x0 <= 0:
                 raise ValidationError(
                     "The Log-normal's distribution's shape parameter, sigma, "
                     "must be > 0.")
 
     def __str__(self):
-        return "parameter object with: function=%r, x0=%r, x1=%r, x2=%r," \
+        return "ParameterModel object with: function=%r, x0=%r, x1=%r, x2=%r," \
                " dependency=%r, name=%r" % \
                (self.function, self.x0, self.x1, self.x2,
                 self.dependency, self.name)
