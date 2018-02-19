f24e815b28c00f46b61699ff230c5bfa90c11f3b
Andreas <a.haselsteiner@uni-bremen.de>
2018-02-19 17:40:47 +0100

    Implement test for user profile, #88

--- tests/test_user_view.py
+++ tests/test_user_view.py
@@ -48,3 +48,24 @@ class UserTestCase(TestCase):
         response = self.client.get(reverse('user:change-password'), follow=True)
         self.assertContains(response, "Edit",
                             status_code = 200)
+
+    def test_show_user_profile(self):
+
+        # Without login
+        response = self.client.get(reverse('user:profile'), follow=True)
+        self.assertContains(response, "Please login to use ViroCon",
+                            status_code = 200)
+
+        # With login
+        self.client.post(reverse('user:create'),
+                           {'username' : 'max_mustermann',
+                            'email' : 'max.mustermann@gmail.com',
+                            'first_name' : 'Max',
+                            'last_name' : 'Mustermann',
+                            'organisation' : 'Musterfirma',
+                            'type_of_use' : 'commercial',
+                            'password1' : 'Musterpasswort2018',
+                            'password2' : 'Musterpasswort2018'})
+        response = self.client.get(reverse('user:profile'), follow=True)
+        self.assertContains(response, "Mustermann",
+                            status_code = 200)
