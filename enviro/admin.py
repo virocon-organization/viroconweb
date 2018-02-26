a37e95239e72d3ab27897f9432661076a75167c6
Andreas <a.haselsteiner@uni-bremen.de>
2018-02-26 18:50:43 +0100

    Make new DjangoModels available to the administrator view, #65

--- enviro/admin.py
+++ enviro/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import MeasureFileModel
-from .models import ProbabilisticModel, DistributionModel, ParameterModel
+from .models import ProbabilisticModel, DistributionModel, ParameterModel, \
+    EnvironmentalContour, AdditionalContourOption, ContourPath, \
+    ExtremeEnvDesignCondition, EEDCScalar
 
 
 # Register your models here.
@@ -8,3 +10,8 @@ admin.site.register(MeasureFileModel),
 admin.site.register(ProbabilisticModel),
 admin.site.register(DistributionModel),
 admin.site.register(ParameterModel),
+admin.site.register(EnvironmentalContour),
+admin.site.register(AdditionalContourOption),
+admin.site.register(ContourPath),
+admin.site.register(ExtremeEnvDesignCondition),
+admin.site.register(EEDCScalar)
--- enviro/models.py
+++ enviro/models.py
@@ -180,6 +180,24 @@ class EnvironmentalContour(models.Model):
     probabilistic_model = models.ForeignKey(ProbabilisticModel,
                                      on_delete=models.CASCADE)
 
+class AdditionalContourOption(models.Model):
+    """
+    Additional options describing how an environmental contour was created.
+
+    Since different environmental contour methods are available some options
+    are only applicable to one method. Consequently, AdditionalContourOption
+    can be used as a dictionary to specify additional options.
+
+    Idea how this model works is based on this stackoverflow post: https://stack
+    overflow.com/questions/402217/how-to-store-a-dictionary-on-a-django-model
+    """
+    # Key can for example be "Number of points on the contour"
+    option_key = models.CharField(max_length=240, null=True)
+    # Then value can for example be "50"
+    option_value = models.CharField(max_length=240, null=True)
+    environmental_contour = models.ForeignKey(EnvironmentalContour,
+                                              on_delete=models.CASCADE)
+
 
 class ContourPath(models.Model):
     """
@@ -217,21 +235,3 @@ class EEDCScalar(models.Model):
     x = models.DecimalField(decimal_places=5, max_digits=10, null=True)
     EEDC = models.ForeignKey(ExtremeEnvDesignCondition,
                                      on_delete=models.CASCADE)
-
-class AdditionalContourOption(models.Model):
-    """
-    Additional options describing how an environmental contour was created.
-
-    Since different environmental contour methods are available some options
-    are only applicable to one method. Consequently, AdditionalContourOption
-    can be used as a dictionary to specify additional options.
-
-    Idea how this model works is based on this stackoverflow post: https://stack
-    overflow.com/questions/402217/how-to-store-a-dictionary-on-a-django-model
-    """
-    # Key can for example be "Number of points on the contour"
-    option_key = models.CharField(max_length=240, null=True)
-    # Then value can for example be "50"
-    option_value = models.CharField(max_length=240, null=True)
-    environmental_contour = models.ForeignKey(EnvironmentalContour,
-                                              on_delete=models.CASCADE)
