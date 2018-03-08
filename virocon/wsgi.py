80f847f602a165cb0fea4a3709fdab333cd96736
Andreas <a.haselsteiner@uni-bremen.de>
2018-03-08 19:53:24 +0100

    Update travis.yml and wsgi to new package name, #82

--- virocon/wsgi.py
+++ virocon/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for EngineeringApp project.
+WSGI config for ViroCon.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EngineeringApp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "virocon.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
