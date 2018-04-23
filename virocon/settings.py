4322324e34a75ee84bff613293a1b3a5b1918b8a
Andreas <a.haselsteiner@uni-bremen.de>
2018-04-23 17:45:08 +0200

    Try to find error by enabling logging

--- virocon/settings.py
+++ virocon/settings.py
@@ -15,6 +15,42 @@ import dj_database_url
 import random
 import string
 
+
+# Thanks to: https://stackoverflow.com/questions/15128135/setting-debug-
+# false-causes-500-error
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'MYAPP': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
