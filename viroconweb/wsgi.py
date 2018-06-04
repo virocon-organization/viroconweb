214efbe54403297e1c60085c5be73ab28741734e
ahaselsteiner <a.haselsteiner@uni-bremen.de>
2018-06-04 21:57:02 +0200

    Redirect heroku to viroconweb.com, #165

--- viroconweb/wsgi.py
+++ viroconweb/wsgi.py
@@ -14,5 +14,13 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "viroconweb.settings")
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# To redirect from the heroku domain to the custom domain. This code block is
+# based on: https://stackoverflow.com/questions/2285879/how-do-i-redirect-
+# domain-com-to-www-domain-com-under-django
+_application = get_wsgi_application()
+_application = DjangoWhiteNoise(_application)
+def application(environ, start_response):
+    if '.herokuapp.com' in environ['HTTP_HOST']:
+        start_response('301 Redirect', [('Location', 'http://www.viroconweb.com/'),])
+        return []
+    return _application(environ, start_response)
