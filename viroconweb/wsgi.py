ded03786615cd07a281210b207fc235999dce177
Andreas <a.haselsteiner@uni-bremen.de>
2018-06-05 13:40:19 +0200

    Remove redirect from heroku domain to custom domain as it might cause memory leak, #165

--- viroconweb/wsgi.py
+++ viroconweb/wsgi.py
@@ -14,14 +14,16 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "viroconweb.settings")
 
-# Redirect from the heroku domain to the custom domain. This code block is
-# based on: https://stackoverflow.com/questions/2285879/how-do-i-redirect-
-# domain-com-to-www-domain-com-under-django
-_application = get_wsgi_application()
-_application = DjangoWhiteNoise(_application)
-def application(environ, start_response):
-    if '.herokuapp.com' in environ['HTTP_HOST']:
-        start_response('301 Redirect',
-                       [('Location', 'http://www.viroconweb.com/'),])
-        return []
-    return _application(environ, start_response)
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+# # Redirect from the heroku domain to the custom domain. This code block is
+# # based on: https://stackoverflow.com/questions/2285879/how-do-i-redirect-
+# # domain-com-to-www-domain-com-under-django
+# _application = get_wsgi_application()
+# _application = DjangoWhiteNoise(_application)
+# def application(environ, start_response):
+#     if '.herokuapp.com' in environ['HTTP_HOST']:
+#         start_response('301 Redirect',
+#                        [('Location', 'http://www.viroconweb.com/'),])
+#         return []
+#     return _application(environ, start_response)
